20de000788bfd5f52f3b8f247c71ed6ff3a21f35
Jim Sweeney <jim.sweeney.ios@gmail.com>
2025-03-06 20:08:21 -0500

    Second Commit

---
+++ weatherappTests/weatherappTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappTests.swift
+//  weatherappTests
+//
+//  Created by James Sweeney on 3/6/25.
+//
+
+import Testing
+@testable import weatherapp
+
+struct weatherappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ weatherappUITests/weatherappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  weatherappUITestsLaunchTests.swift
+//  weatherappUITests
+//
+//  Created by James Sweeney on 3/6/25.
+//
+
+import XCTest
+
+final class weatherappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
